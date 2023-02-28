07c35837a2813adef0bc5972045439bce513d428
hosecloud <4osecloud@gmail.com>
2023-02-28 23:30:58 +0900

    Reset App.js

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -1,23 +1,7 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destoryed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
