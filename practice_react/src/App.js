14dd571f4abf7e3f71e3c9d709af910ca84d4eff
hosecloud <4osecloud@gmail.com>
2023-02-28 21:30:52 +0900

    Practice useEffect conditional

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -1,18 +1,27 @@
 import {useState, useEffect} from "react";
 
-import styles from "./App.module.css";
-
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("Always run all the time!");
-  const iRunOnlyOnce = () => {
+  useEffect(() => {
     console.log("run once time!");
-  }
-  useEffect(iRunOnlyOnce, []);
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5)
+      console.log("search for ", keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>{counter}</h1>
+      <input
+        type="text"
+        placeholder="Search here..."
+        value={keyword}
+        onChange={onChange}
+      />
+      <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
   );
