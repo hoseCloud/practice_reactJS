b5d38024b258ba65bd7adb25f854abe10a9526b8
hosecloud <4osecloud@gmail.com>
2023-02-28 18:24:28 +0900

    Practice useEffect

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -1,11 +1,19 @@
-import Button from "./Button";
+import {useState, useEffect} from "react";
+
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("Always run all the time!");
+  const iRunOnlyOnce = () => {
+    console.log("run once time!");
+  }
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Just continue"}/>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
