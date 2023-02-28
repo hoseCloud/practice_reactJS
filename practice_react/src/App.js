b5bb108d494b56ed99ba324706869c49887d5576
hosecloud <4osecloud@gmail.com>
2023-03-01 03:02:00 +0900

    Add screen and router

--- practice_react/src/App.js
+++ practice_react/src/App.js
@@ -1,7 +1,16 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
