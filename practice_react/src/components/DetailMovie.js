7264fb9b9a5ff2357b0c42f5235ebcb90787034b
hosecloud <4osecloud@gmail.com>
2023-03-01 18:01:59 +0900

    Add movie detail

---
+++ practice_react/src/components/DetailMovie.js
@@ -0,0 +1,43 @@
+import { PropTypes } from "prop-types";
+
+// big img, title, year, runtime, rating, summary, genres
+
+function DetailMovie({
+  coverImage,
+  title,
+  year,
+  runtime,
+  rating,
+  summary,
+  genres,
+}) {
+  return (
+    <div>
+      <img src={coverImage} alt={title} />
+      <h2>
+        {title} ({year})
+      </h2>
+      <h3>
+        runtime: {runtime}, rating: {rating}
+      </h3>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+DetailMovie.prototype = {
+  coverImage: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  runtime: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default DetailMovie;
--- practice_react/src/routes/Detail.js
+++ practice_react/src/routes/Detail.js
@@ -1,22 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import DetailMovie from "../components/DetailMovie";
 
 // Add loading
 // Add to show detail in page
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
   const { id } = useParams();
   const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    console.log(json);
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    );
+    const json = await response.json();
+
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
-  }, []);
+  });
 
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <DetailMovie
+          coverImage={movie.large_cover_image}
+          title={movie.title}
+          year={movie.year}
+          runtime={movie.runtime}
+          rating={movie.rating}
+          summary={movie.description_full}
+          genres={movie.genres}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Detail;
