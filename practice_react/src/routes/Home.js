b998c84fc338c13f0f0ce1bbb9114c82440a7fdc
hosecloud <4osecloud@gmail.com>
2023-03-03 03:19:46 +0900

    Add grid in Home.js

--- practice_react/src/routes/Home.js
+++ practice_react/src/routes/Home.js
@@ -23,7 +23,7 @@ function Home() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {movies.map((movie) => (
             <Movie
               key={movie.id}
