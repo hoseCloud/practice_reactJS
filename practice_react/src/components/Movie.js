97f85b2d1b9ac8e353cc7bd8d3ab0ca1b4874e61
hosecloud <4osecloud@gmail.com>
2023-03-03 03:19:36 +0900

    Add Card in Movei.js

--- practice_react/src/components/Movie.js
+++ practice_react/src/components/Movie.js
@@ -2,18 +2,28 @@ import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
 
 function Movie({ id, coverImage, title, summary, genres }) {
+  if (summary.length > 500) {
+    summary = summary.slice(0, 500) + "...";
+  }
   return (
-    <div>
-      <img src={coverImage} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full p-2" src={coverImage} alt={title} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </div>
+        <p className="text-gray-700 text-base">{summary}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
         {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
+          <span
+            key={genre}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-1"
+          >
+            {genre}
+          </span>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
